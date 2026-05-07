b8a689aa73a99cdd71eb3141010e3f1788ba59e8
ahmads2006 <ahmads2006@example.com>
2026-05-07 14:22:24 +0300

    feat: implement broadcast events for group, private, and support messaging

--- app/Events/SupportMessageSent.php
+++ app/Events/SupportMessageSent.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\SupportMessage;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SupportMessageSent implements ShouldBroadcast
+class SupportMessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
